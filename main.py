a4d73c88bf730bcc87faaed38151a99cd92f0f6f
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 17:33:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# this will be to automate a form i must fill multiple times a day
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import urls
+from selenium.webdriver.remote.webdriver import WebDriver
 
-chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-chrome_options.add_argument(f"user-data-dir={urls.CHROME_USER_DIR}")
-# setting chrome driver path
-driver = webdriver.Chrome(options=chrome_options, keep_alive=True)
+# executor_url = driver.command_executor._url
+# session_id = driver.session_id
 
-driver.get(urls.link)
-print(driver.title)
+
+def attach_to_session(executor_url, session_id):
+    original_execute = WebDriver.execute
+
+    def new_command_execute(self, command, params=None):
+        if command == "newSession":
+            # Mock the response
+            return {"success": 0, "value": None, "sessionId": session_id}
+        else:
+            return original_execute(self, command, params)
+
+    # Patch the function before creating the driver object
+    WebDriver.execute = new_command_execute
+    driver = webdriver.Remote(command_executor=executor_url)
+    driver.session_id = session_id
+    # Replace the patched function with original function
+    WebDriver.execute = original_execute
+    return driver
+
+
+bro = attach_to_session("http://127.0.0.1:64092", "8de24f3bfbec01ba0d82a7946df1d1c3")
+bro.get("http://ya.ru/")
