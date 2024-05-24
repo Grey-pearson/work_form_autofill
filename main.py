40a93c25c7b07f031a60ee694fa7c755e4284955
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 16:36:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # this will be to automate a form i must fill multiple times a day
 from selenium import webdriver
-from urls import LINK, CHROME_USER_DIR
+import urls
 
 options = webdriver.ChromeOptions()
-options.add_argument(f"user-data-dir={CHROME_USER_DIR}")
+options.add_argument(f"user-data-dir={urls.CHROME_USER_DIR}")
 options.add_argument("profile-directory=Default")
 
 driver = webdriver.Chrome(options=options)
-driver.get(LINK)
+driver.get(urls.LINK)
