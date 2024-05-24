547c0df53d02b015fa57cf1bf83ef71f945c22e1
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 16:16:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # this will be to automate a form i must fill multiple times a day
 from selenium import webdriver
+from urls import link
+
 
 driver = webdriver.Chrome()
-driver.get()
+driver.get(link)
