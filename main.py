a8ad9696072d1d501ecbd933beeee4dd31e5f05d
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 16:35:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from urls import LINK, CHROME_USER_DIR
 options = webdriver.ChromeOptions()
 options.add_argument(f"user-data-dir={CHROME_USER_DIR}")
 options.add_argument("profile-directory=Default")
+
+driver = webdriver.Chrome(options=options)
+driver.get(LINK)
