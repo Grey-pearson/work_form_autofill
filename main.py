906806aa0d1f8749258553bee3146bd9e0a65276
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 16:48:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import urls
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 chrome_options.add_argument(f"user-data-dir={urls.CHROME_USER_DIR}")
 chrome_options.add_argument("profile-directory=Default")
+chrome_driver = "C:\chromedriver.exe"
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(urls.LINK)
+driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
+print(driver.title)
