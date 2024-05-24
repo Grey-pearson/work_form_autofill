dc8a827eccca0e444f23938cddbee7e3966367bf
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 17:09:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import urls
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 chrome_options.add_argument(f"user-data-dir={urls.CHROME_USER_DIR}")
-chrome_options.add_argument("profile-directory=Default")
-# chrome_options.add_experimental_option("detach", True)
-chrome_driver = "C:\chromedriver.exe"
-
-driver = webdriver.Chrome(options=chrome_options, keep_alive=True)
-print(driver.title)
-
-driver.get(urls.LINK)
-print("done")
+# setting chrome driver path
+driver = webdriver.Chrome(executable_path)
