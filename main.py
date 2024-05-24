16175842f02c30f20df41fd9a770ce7036045917
Grey Pearson <grey.pearson@lvt.com>
2024-05-24 17:09:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 chrome_options.add_argument(f"user-data-dir={urls.CHROME_USER_DIR}")
 # setting chrome driver path
-driver = webdriver.Chrome(executable_path)
+driver = webdriver.Chrome(
+    executable_path="C:\chromedriver.exe", options=chrome_options, keep_alive=True
+)
+
+driver.get(urls.link)
+print(driver.title)
