c9279aad5d31e9754b3d9a2ed3e8388a52ec3a61
Grey Pearson <grey.pearson@lvt.com>
2024-05-27 12:44:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(LINK)
 
 
-# click on img name ServiceNow logo
-wait = WebDriverWait(driver, timeout=30)
-wait.until(lambda d: service_now.is_displayed())
-service_now = driver.find_element(by="name", value="ServiceNow logo")
-print(service_now)
-# click on button name rquest catolog
-# find element to select for config request
-# fill out form
+def click_service_now(driver):
+    # click on img name ServiceNow logo
+    service_now = driver.find_element(By.NAME, value="ServiceNow logo")
+    # driver.find_element(By.NAME, "revealed")
 
-# im going to create tkinter ui for this as well probably
-driver.close()
+    wait = WebDriverWait(driver, timeout=30)
+    wait.until(lambda d: service_now.is_displayed())
+    print(service_now)
+    # click on button name rquest catolog
+    # find element to select for config request
+    # fill out form
+
+    # im going to create tkinter ui for this as well probably
+    driver.close()
+
+
+click_service_now(driver)
