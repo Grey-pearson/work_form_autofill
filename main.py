17b591c4c5f66ed42bd97b0c3f667530cae90efe
Grey Pearson <grey.pearson@lvt.com>
2024-05-27 09:42:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(LINK)
 
+# click on img name ServiceNow logo
+
+# click on button name rquest catolog
+# find element to select for config request
+# fill out form
 
 # im going to create tkinter ui for this as well probably
+driver.close()
