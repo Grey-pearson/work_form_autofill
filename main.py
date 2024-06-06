19121b2536c66c192f73c1153b34bf97d5543b86
Grey Pearson <grey.pearson@lvt.com>
2024-06-06 11:06:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ browser_control = BrowserControl()
 
 browser_control.sign_in()
 
-browser_control.open_service_now()
-browser_control.open_old_vms()
+# browser_control.open_service_now()
+# browser_control.open_old_vms()
 
 # browser_control.close()
