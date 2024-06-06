d74f50750098b48838778c36c63bbbd2c80a4210
Grey Pearson <grey.pearson@lvt.com>
2024-06-06 10:57:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,6 @@ from urls import LINK
 from Class.CamConfig import BrowserControl
 
 
-def click_service_now():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("detach", True)
-
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(LINK)
-    driver.find_element(By.LINK_TEXT, value="select")
-    driver.implicitly_wait(60)
-    # click on img name ServiceNow logo
-    service_now = driver.find_element(By.LINK_TEXT, value="ServiceNow")
-    # driver.find_element(By.NAME, "revealed")
-
-    wait = WebDriverWait(driver, timeout=60)
-    wait.until(lambda d: service_now.is_displayed())
-
-    print(service_now)
-    # click on button name rquest catolog
-    # find element to select for config request
-    # fill out form
-
-    # im going to create tkinter ui for this as well probably
-
-
-# click_service_now()
-
 browser_control = BrowserControl()
 
 browser_control.sign_in()
